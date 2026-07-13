ef31d076398605140bbdfce3e636487a7eb4d1e5
dlifad <zainurfadli2005@gmail.com>
2026-07-14 06:17:05 +0700

    update data

--- script.js
+++ script.js
@@ -1,21 +1,6 @@
 const umkmGrid = document.getElementById("umkmGrid");
 const searchInput = document.getElementById("searchInput");
 
-function getSortedData(data) {
-  return [...data].sort((a, b) => {
-    const rwA = Number(a.rw) || 0;
-    const rwB = Number(b.rw) || 0;
-    const rtA = Number(a.rt) || 0;
-    const rtB = Number(b.rt) || 0;
-    const noA = Number(a.noUrut || a.id) || 0;
-    const noB = Number(b.noUrut || b.id) || 0;
-
-    if (rwA !== rwB) return rwA - rwB;
-    if (rtA !== rtB) return rtA - rtB;
-    return noA - noB;
-  });
-}
-
 function getJenisProduksiText(data) {
   if (!data.jenisProduksi) return "-";
 
@@ -63,9 +48,8 @@ function renderUMKMList(keyword = "") {
   if (!umkmGrid) return;
 
   const normalizedKeyword = keyword.toLowerCase();
-  const sortedData = getSortedData(dataUMKM);
 
-  const filteredData = sortedData.filter((pandaiBesi) => {
+  const filteredData = dataUMKM.filter((pandaiBesi) => {
     const alamat =
       pandaiBesi.lokasi ||
       `RT ${pandaiBesi.rt || "-"} / RW ${pandaiBesi.rw || "-"}`;
@@ -98,7 +82,7 @@ function renderUMKMList(keyword = "") {
     item.className = "umkm-list-item";
 
     item.innerHTML = `
-      <div class="umkm-number">${pandaiBesi.noUrut || pandaiBesi.id}</div>
+      <div class="umkm-number">${pandaiBesi.noUrut}</div>
 
       <div class="umkm-list-content">
         <h3>Pandai Besi ${pandaiBesi.nama || "-"}</h3>
